c29af423ac5d3a116343c2c8513abda3f1036058
wwsouthard <wsouthard@my365.bellevue.edu>
2026-01-30 12:08:23 -0600

    python file created

---
+++ week_2/exercise2-3.py
@@ -0,0 +1,10 @@
+"""
+File Name: quote.py
+Author: Will Southard
+Date: February 2026
+Description: This program prints a quote along with its author.
+"""
+
+# Print a quote and credit the original author
+print("“The best way to predict the future is to invent it.”")
+print("- Alan Kay")
